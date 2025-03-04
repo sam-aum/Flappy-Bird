5d0b01083c164e68677ac13ed07754c2840beadf
sam-aum <psaum01@gmail.com>
2025-03-03 22:02:34 -0600

    window resizing

--- bird0/main1.lua
+++ bird0/main1.lua
@@ -13,4 +13,16 @@ function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     love.window.setTitle('Flappy Bird')
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })      
 end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+
