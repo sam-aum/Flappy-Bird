47ca9b2434a0b173f5c0ffa3464ab9d2ab10a56a
sam-aum <psaum01@gmail.com>
2025-03-25 09:31:01 -0500

    changed bird7 collision

--- bird7/Bird.lua
+++ bird7/Bird.lua
@@ -35,8 +35,8 @@ function Bird:collides(pipe)
     -- the 4's are right and bottom offsets
     -- both offsets are used to shrink the bounding box to give the player
     -- a little bit of leeway with the collision
-    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
-        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+    if (self.x + 2) + (self.width - 8) >= pipe.x and self.x + 2 <= pipe.x + pipe.width then
+        if (self.y + 2) + (self.height - 8) >= pipe.y and self.y + 2 <= pipe.y + pipe.height then
             return true
         end
     end
