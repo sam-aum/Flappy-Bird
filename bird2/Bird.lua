77564e5e94a03ce1b3c33295d788ffdafa07e2dc
Sam Aum <psaum01@gmail.com>
2026-07-01 16:45:49 -0500

    bird2

--- bird2/Bird.lua
+++ bird2/Bird.lua
@@ -1,11 +1,3 @@
---[[
-    Bird Class
-
-    The Bird is what we control in the game via clicking or the space bar; whenever we press either,
-    the bird will flap and go up a little bit, where it will then be affected by gravity. If the bird hits
-    the ground or a pipe, the game is over.
-]]
-
 Bird = Class{}
 
 function Bird:init()
