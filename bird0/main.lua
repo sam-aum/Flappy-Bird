cfd50fc1f4f83294d00c0f33796fe96896b9aca3
Sam Aum <psaum01@gmail.com>
2026-06-30 21:34:53 -0500

    practice pong 0

--- bird0/main.lua
+++ bird0/main.lua
@@ -1,39 +1,31 @@
---[[
-    GD50
-    Flappy Bird Remake
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
-    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
-    the screen, making the player's bird avatar flap its wings and move upwards slightly. 
-    A variant of popular games like "Helicopter Game" that floated around the internet
-    for years prior. Illustrates some of the most basic procedural generation of game
-    levels possible as by having pipes stick out of the ground by varying amounts, acting
-    as an infinitely generated obstacle course for the player.
-]]
-
--- virtual resolution handling library
 push = require 'push'
 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
-    -- initialize our nearest-neighbor filter
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-
-    -- app window title
     love.window.setTitle('Fifty Bird')
 
-    -- initialize our virtual resolution
-    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    background = love.graphics.newImage('background.png')
+    ground = love.graphics.newImage('ground.png')
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         vsync = true,
         fullscreen = false,
         resizable = true
     })
+
+    -- initialize our virtual resolution
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
 end
 
 function love.resize(w, h)
-    push:resize(w, h)
+    push.resize(w, h)
 end
 
 function love.keypressed(key)
@@ -43,13 +35,13 @@ function love.keypressed(key)
 end
 
 function love.draw()
-    push:start()
-    
+    push.start()
+
     -- draw the background starting at top left (0, 0)
     love.graphics.draw(background, 0, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
     love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
-    
-    push:finish()
+
+    push.finish()
 end
