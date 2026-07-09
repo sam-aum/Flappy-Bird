d8912c0a9b8f345f705b13cfd4dfc0d0845bec86
Sam Aum <psaum01@gmail.com>
2026-07-09 17:32:41 -0500

    bird 7 finished

--- bird7 Practice/Bird.lua
+++ bird7 Practice/Bird.lua
@@ -1,13 +1,3 @@
---[[
-    Bird Class
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    The Bird is what we control in the game via clicking or the space bar; whenever we press either,
-    the bird will flap and go up a little bit, where it will then be affected by gravity. If the bird hits
-    the ground or a pipe, the game is over.
-]]
-
 Bird = Class{}
 
 local GRAVITY = 980
@@ -24,6 +14,25 @@ function Bird:init()
 
     -- Y velocity; gravity
     self.dy = 0
+end
+
+
+--[[
+    AABB collision that expects a pipe, which will have an X and Y and reference
+    global pipe width and height values.
+]]
+function Bird:collides(pipe)
+    -- the 2's are left and top offsets
+    -- the 4's are right and bottom offsets
+    -- both offsets are used to shrink the bounding box to give the player
+    -- a little bit of leeway with the collision
+    if (self.x + 2) + (self.width - 4) >= pipe.x and self.x + 2 <= pipe.x + PIPE_WIDTH then
+        if (self.y + 2) + (self.height - 4) >= pipe.y and self.y + 2 <= pipe.y + PIPE_HEIGHT then
+            return true
+        end
+    end
+
+    return false
 end
 
 function Bird:update(dt)
