829208c492270b2b5ddbb1440dc4cde7b44ea886
Sam Aum <psaum01@gmail.com>
2026-07-10 21:01:47 -0500

    added state file

---
+++ bird8 Practice/main.lua
@@ -0,0 +1,189 @@
+push = require 'push'
+
+Class = require 'class'
+
+-- bird class we've written
+require 'Bird'
+
+require 'Pipe'
+
+-- class representing pair of pipes together
+require 'PipePair'    
+
+-- all code related to game state and state machines
+require 'StateMachine'
+require 'states.BaseState'
+require 'states.PlayState'
+require 'states.TitleScreenState'
+
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- background starting scroll location (X axis)
+local backgroundScroll = 0
+
+-- ground starting scroll location (X axis)
+local groundScroll = 0
+
+-- speed at which we should scroll our images, scaled by dt
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which we should loop our background back to X 0
+local BACKGROUND_LOOPING_POINT = 413
+
+-- our table of spawning PipePairs
+local pipePairs = {}
+
+-- our timer for spawning pipes
+local spawnTimer = 0
+
+
+-- initialize our last recorded Y value for a gap placement to base other gaps off of
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+-- scrolling variable to pause the game when we collide with a pipe
+local scrolling = true
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.window.setTitle('Fifty Bird')
+
+    background = love.graphics.newImage('background.png')
+    ground = love.graphics.newImage('ground.png')
+
+    bird = Bird()
+
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    -- initialize our virtual resolution
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, { upscale = 'normal' })
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['play'] = function() return PlayState() end,
+    }
+    gStateMachine:change('title')
+
+    -- initialize input table
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push.resize(w, h)
+end
+
+function love.keypressed(key)
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
+
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
+    New function used to check our global input table for keys we activated during
+    this frame, looked up by their string value.
+]]
+function love.keyboard.wasPressed(key)
+    if love.keyboard.keysPressed[key] then
+        return true
+    else
+        return false
+    end
+end
+
+function love.update(dt)
+    if scrolling then
+        -- scroll background by preset speed * dt, looping back to 0 after the looping point
+        backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
+            % BACKGROUND_LOOPING_POINT
+
+        -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
+            % VIRTUAL_WIDTH 
+
+        spawnTimer = spawnTimer + dt
+
+        -- spawn a new Pipe if the timer is past 2 seconds
+        if spawnTimer > 3 then
+            -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
+            -- no higher than 10 pixels below the top edge of the screen,
+            -- and no lower than a gap length (90 pixels) from the bottom
+            local y = math.max(-PIPE_HEIGHT + 10,
+                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            lastY = y
+
+            table.insert(pipePairs, PipePair(y))
+            spawnTimer = 0
+        end
+
+
+        bird:update(dt)
+
+        -- for every pipe pair in the scene...
+        for k, pair in pairs(pipePairs) do
+            pair:update(dt)
+
+            -- check to see if bird collided with pipe
+            for l, pipe in pairs(pair.pipes) do
+                if bird:collides(pipe) then
+                    -- pause the game to show collision
+                    scrolling = false
+                end
+            end
+
+            -- if pipe is no longer visible past left edge, remove it from scene
+            if pair.x < -PIPE_WIDTH then
+                pair.remove = true
+            end
+        end
+
+        -- remove any flagged pipes
+        -- we need this second loop, rather than deleting in the previous loop, because
+        -- modifying the table in-place without explicit keys will result in skipping the
+        -- next pipe, since all implicit keys (numerical indices) are automatically shifted
+        -- down after a table removal
+        for k, pair in pairs(pipePairs) do
+            if pair.remove then
+                table.remove(pipePairs, k)
+            end
+        end
+
+            -- reset input table
+            love.keyboard.keysPressed = {}
+    end
+end
+
+function love.draw()
+    push.start()
+
+    -- draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+
+    -- render all the pipes in our scene
+    for k, pipe in pairs(pipePairs) do
+        pipe:render()
+    end
+
+    -- draw the ground on top of the background, toward the bottom of the screen,
+    -- at its negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+
+
+    bird:render()
+    
+    push.finish()
+end
